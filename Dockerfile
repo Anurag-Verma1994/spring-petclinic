5e6257608189cf1de92e781fa0bea24f4b3070f2
Anurag-Verma1994 <vermaanurag1794@gmail.com>
2025-08-31 21:17:17 +0200

    testing

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,5 @@ WORKDIR /app
 COPY --from=builder /app/target/*.jar /app/app.jar
 
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "/app/app.jar"]
 
 
--- terraform/modules/cloud-run/main.tf
+++ terraform/modules/cloud-run/main.tf
@@ -1,20 +1,3 @@
-# Get database connection info from Secret Manager
-data "google_secret_manager_secret_version" "db_connection" {
-  project = var.project_id
-  secret  = "petclinic-${var.environment}-db-connection"
-  version = "latest"
-}
-
-data "google_secret_manager_secret_version" "db_password" {
-  project = var.project_id
-  secret  = "petclinic-${var.environment}-db-password"
-  version = "latest"
-}
-
-locals {
-  db_connection = jsondecode(data.google_secret_manager_secret_version.db_connection.secret_data)
-}
-
 resource "google_cloud_run_service" "service" {
   name     = var.service_name
   location = var.region
@@ -29,41 +12,6 @@ resource "google_cloud_run_service" "service" {
           limits = {
             cpu    = "4.0"
             memory = "4Gi"
-          }
-        }
-
-        env {
-          name = "SPRING_PROFILES_ACTIVE"
-          value = "postgres"
-        }
-
-        env {
-          name  = "POSTGRES_HOST"
-          value = local.db_connection.host
-        }
-        
-        env {
-          name  = "POSTGRES_PORT"
-          value = tostring(local.db_connection.port)
-        }
-
-        env {
-          name  = "POSTGRES_DB"
-          value = local.db_connection.database
-        }
-
-        env {
-          name  = "POSTGRES_USER"
-          value = local.db_connection.username
-        }
-
-        env {
-          name  = "POSTGRES_PASS"
-          value_from {
-            secret_key_ref {
-              name = "petclinic-${var.environment}-db-password"
-              key  = "latest"
-            }
           }
         }
 
@@ -86,7 +34,6 @@ resource "google_cloud_run_service" "service" {
         "autoscaling.knative.dev/maxScale"        = var.max_instances
         "run.googleapis.com/cpu-throttling"       = "false"  # CPU always allocated
         "run.googleapis.com/startup-cpu-boost"    = "true"   # CPU boost during startup
-        "run.googleapis.com/cloudsql-instances"   = "${var.project_id}:europe-west4:petclinic-${var.environment}-primary"
       }
     }
   }
